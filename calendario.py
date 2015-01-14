9fcf2f50862c5234fb90ce3404557c016c737617
MPGPiero <MPGPiero@users.noreply.github.com>
2015-01-14 08:39:26 +0100

    Update calendario.py
    file  type excel

--- calendario.py
+++ calendario.py
@@ -23,8 +23,8 @@ def converti_data(data):
 def restituisci_ore(data):
     return float(data.seconds/3600)
 
-# leggi la tabella
-foglio = pd.read_csv('calendar.csv')
+# leggi la tabella excel  modifica Piero
+foglio = pd.read_excel('calendar.xlsx')
 
 # array dove mettere le ore lavorate
 ore_lavorate = []
